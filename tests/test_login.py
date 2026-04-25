e1bbd46f3242ccd1f8676ba4df1a45184132abcd
sanak101 <sanakawan01@gmail.com>
2026-04-25 20:49:19 +0500

    testing valid invalid login test cases

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,1 +1,45 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+from utils.driver_setup import get_driver
+
+def test_valid_login():
+    driver = get_driver()
+    wait = WebDriverWait(driver,10)
+
+    driver.get("https://the-internet.herokuapp.com/login")
+
+    wait.until(EC.visibility_of_element_located((By.ID, "username"))).send_keys("tomsmith")
+    driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
+    driver.find_element(By.CLASS_NAME, "radius").click()
+
+    message = wait.until(
+        EC.visibility_of_element_located((By.ID, "flash"))
+    ).text
+
+    driver.quit()
+
+
+
+def test_invalid_login():
+    driver = get_driver()
+    wait = WebDriverWait(driver, 10)
+
+    driver.get("https://the-internet.herokuapp.com/login")
+
+    wait.until(EC.visibility_of_element_located((By.ID, "username"))).send_keys("wrong")
+    driver.find_element(By.ID, "password").send_keys("wrong")
+    driver.find_element(By.CLASS_NAME, "radius").click()
+
+    message = wait.until(
+        EC.visibility_of_element_located((By.ID, "flash"))
+    ).text
+
+    print("Invalid Login:", "PASS" if "invalid" in message else "FAIL")
+
+    driver.quit()
+
+
+test_valid_login()
+test_invalid_login()
--- utils/driver_setup.py
+++ utils/driver_setup.py
@@ -1,1 +1,7 @@
+from selenium import webdriver
 
+def get_driver():
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+    return driver
